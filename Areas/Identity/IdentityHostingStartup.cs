7db619c472c6566c158128cb4601bb7dc6b51046
Brandon Prange <bprange93@gmail.com>
2020-11-23 13:36:13 -0600

    Roles implemented. Controllers for Parent and child to be created.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlannerProject.Data;
+
+[assembly: HostingStartup(typeof(PlannerProject.Areas.Identity.IdentityHostingStartup))]
+namespace PlannerProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
