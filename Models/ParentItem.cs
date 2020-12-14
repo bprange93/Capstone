fc7f5fb287eedddaddb46ebda4bd6bc295369469
Brandon Prange <bprange93@gmail.com>
2020-12-14 14:59:57 -0600

    These classes were added for a purpose but a better way was found. May use later.

---
+++ Models/ParentItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerProject.Models
+{
+    public class ParentItem
+    {
+        [Key]
+        public string Name { get; set; }
+        [ForeignKey("ParentsTask")]
+        public int ParentsTaskId { get; set; }
+        public ParentsTask ParentsTask { get; set; }
+    }
+}
